1e1b71eef600b696e2e5459403c41b3d5e0501d4
Marcel Wiessler <marcel@gaisterhand.de>
2022-06-07 14:36:05 +0200

    2021 proj

--- package/Editor/Utils.cs
+++ package/Editor/Utils.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
-using UnityEditor.Experimental.SceneManagement;
+
 using UnityEngine.SceneManagement;
 // ReSharper disable CheckNamespace
 
@@ -21,40 +21,40 @@ namespace Needle.ComponentExtension
 		{
 			members = null;
 			return;
-			string[] lines = null;
-			if (PrefabStageUtility.GetCurrentPrefabStage())
-			{
-				// TODO: render in prefab stage
-			}
-			else
-			{
-				var scene = SceneManager.GetActiveScene();
-				lines = File.ReadAllLines(scene.path);
-			}
-
-			if (lines != null)
-			{
-				// TODO: this is the naive version, a component could be multiple times on an object and we also need to check for the gameobject id first and find the correct component etc
-				var foundStart = false;
-				foreach (var line in lines)
-				{
-					if (foundStart && line.StartsWith("---")) break;
-					if (foundStart)
-					{
-						members ??= new List<MemberInfo>();
-						var member = new MemberInfo();
-						members.Add(member);
-						var values = line.Split(':');
-						member.Name = values[0].Trim();
-						member.Value = values[1].Trim();
-						member.Property = obj.FindProperty(member.Name);
-					}
-					else if (line.Contains(identifier))
-					{
-						foundStart = true;
-					}
-				}
-			}
+			// string[] lines = null;
+			// if (UnityEditor.SceneManagement.PrefabStageUtility.GetCurrentPrefabStage())
+			// {
+			// 	// TODO: render in prefab stage
+			// }
+			// else
+			// {
+			// 	var scene = SceneManager.GetActiveScene();
+			// 	lines = File.ReadAllLines(scene.path);
+			// }
+			//
+			// if (lines != null)
+			// {
+			// 	// TODO: this is the naive version, a component could be multiple times on an object and we also need to check for the gameobject id first and find the correct component etc
+			// 	var foundStart = false;
+			// 	foreach (var line in lines)
+			// 	{
+			// 		if (foundStart && line.StartsWith("---")) break;
+			// 		if (foundStart)
+			// 		{
+			// 			members ??= new List<MemberInfo>();
+			// 			var member = new MemberInfo();
+			// 			members.Add(member);
+			// 			var values = line.Split(':');
+			// 			member.Name = values[0].Trim();
+			// 			member.Value = values[1].Trim();
+			// 			member.Property = obj.FindProperty(member.Name);
+			// 		}
+			// 		else if (line.Contains(identifier))
+			// 		{
+			// 			foundStart = true;
+			// 		}
+			// 	}
+			// }
 		}
 	}
 }
