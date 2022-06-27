337bfcb2430d054e7fda879d8444d8f87e8abd3f
Marcel Wiessler <marcel@gaisterhand.de>
2022-06-27 18:14:16 +0200

    Add menu item to show objects with missing scripts

---
+++ package/Editor/HiddenObjectsWithMissingScripts.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Needle
+{
+	public static class HiddenObjectsWithMissingScripts
+	{
+		[MenuItem("Tools/Needle/Show Objects With Missing Scripts")]
+		private static void ShowHiddenObjects()
+		{
+			var gos = Resources.FindObjectsOfTypeAll<GameObject>();
+			var comps = new List<Component>();
+			foreach (var go in gos)
+			{
+				comps.Clear();
+				go.GetComponents(comps);
+				foreach(var comp in comps)
+				{
+					if (comp == null)
+					{
+						go.hideFlags = HideFlags.None;
+					}
+				}
+			}
+		}
+	}
+}
