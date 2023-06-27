a9041a7740d2e322671cb766e4f06b9427ae20b9
Marcel Wiessler <marcel@gaisterhand.de>
2023-06-27 14:17:59 +0200

    internal

---
+++ projects/shared/Runtime/MissingScripts/MyScriptWithProperties.cs
@@ -0,0 +1,20 @@
+// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+//
+// public class MyScriptWithProperties_ : MonoBehaviour
+// {
+//     public float MyFloatValue = 42;
+//     public string MyText = "Hello";
+//     public bool MyBool;
+//     public MySerializedClass MyClass;
+//     public List<int> MyList;
+//     
+//     [Serializable]
+//     public class MySerializedClass
+//     {
+//         public bool MyBool;
+//     }
+//     
+// }
